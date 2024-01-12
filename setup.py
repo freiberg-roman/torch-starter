6d89856fbeaaf7f9e168c2f1520362860ce38248
Roman Freiberg <me@romanfreiberg.com>
2024-01-12 17:50:17 +0100

    add working setup

--- setup.py
+++ setup.py
@@ -1,28 +1,28 @@
-import codecs
 import pathlib
-import re
 
-import pkg_resources
 from setuptools import find_namespace_packages, setup
 
 
-def find_version(*file_paths: str) -> str:
-    with codecs.open(open.path.join(*file_paths), "r") as f:
-        version_file = f.read()
-    version_match = re.search(r"^__version__ = ['\"](^'\"*)['\"])", version_file, re.M)
-    if version_match:
-        return version_match.group(1)
-    raise RuntimeError("Unable to find version string.")
+def parse_requirements_file(path):
+    return [line.rstrip() for line in open(path, "r")]
 
 
-with pathlib.Path("requirements/requirements.txt").open() as f:
-    install_requires = [str(requirement) for requirement in pkg_resources.parse_requirements(f)]
+reqs_main = parse_requirements_file("requirements/requirements.txt")
+reqs_dev = parse_requirements_file("requirements/dev.txt")
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+
+init_str = pathlib.Path("src/__init__.py").read_text()
+version = init_str.split("__version__ = ")[1].rstrip().strip('"')
+
 
 with open("README.md") as f:
     LONG_DESC = f.read()
     setup(
         name="torch-starter",
-        version=find_version("src", "__init__.py"),
+        version=version,
         author="Roman Freiberg",
         author_email="me@romanfreiberg.com",
         description="Torch Starter with Clean Architecture in mind.",
@@ -34,5 +34,8 @@ with open("README.md") as f:
         classifiers=[
             "Programming Language :: Python :: 3.9",
         ],
-        install_requires=install_requires,
+        install_requires=reqs_main,
+        extras_require={
+            "dev": reqs_main + reqs_dev,
+        },
     )
