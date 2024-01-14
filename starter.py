891c8c72401de3bf8fc0dda557bf4acf366c4ccd
Roman Freiberg <me@romanfreiberg.com>
2024-01-14 17:30:47 +0100

    correct order

--- starter.py
+++ starter.py
@@ -1,32 +1,39 @@
-import os
 import argparse
 import fileinput
+import os
 
-def rename_src_directory(package_name):
-    if os.path.exists('src') and os.path.isdir('src'):
-        os.rename('src', package_name)
-    else:
-        print("Directory 'src' not found. Make sure you are in the correct directory.")
 
 def replace_in_python_files(src_dir, package_name):
-    for file in os.listdir(src_dir):
-        if file.endswith('.py'):
-            filepath = os.path.join(src_dir, file)
-            with fileinput.FileInput(filepath, inplace=True) as file:
-                for line in file:
-                    print(line.replace('from src.', f'from {package_name}.'), end='')
+    if os.path.exists(src_dir) and os.path.isdir(src_dir):
+        for file in os.listdir(src_dir):
+            if file.endswith(".py"):
+                filepath = os.path.join(src_dir, file)
+                with fileinput.FileInput(filepath, inplace=True) as file:
+                    for line in file:
+                        print(line.replace("from src.", f"from {package_name}."), end="")
+    else:
+        print(f"Directory '{src_dir}' not found. Make sure you are in the correct directory.")
+
+
+def rename_src_directory(src_dir, package_name):
+    if os.path.exists(src_dir):
+        os.rename(src_dir, package_name)
+    else:
+        print(f"Could not rename '{src_dir}' as it does not exist.")
+
 
 def replace_package_name_placeholder(top_level_dir, package_name):
     for file in os.listdir(top_level_dir):
-        if not file.endswith('.py') and not os.path.isdir(file):
+        if not file.endswith(".py") and not os.path.isdir(file):
             filepath = os.path.join(top_level_dir, file)
             with fileinput.FileInput(filepath, inplace=True) as file:
                 for line in file:
-                    print(line.replace('{{package_name}}', package_name), end='')
+                    print(line.replace("{{package_name}}", package_name), end="")
+
 
 def main():
     parser = argparse.ArgumentParser(description="Configure PyTorch Project")
-    parser.add_argument('-n', '--name', type=str, help='Package name')
+    parser.add_argument("-n", "--name", type=str, help="Package name")
     args = parser.parse_args()
 
     if args.name:
@@ -34,13 +41,14 @@ def main():
     else:
         package_name = input("Enter the package name: ")
 
-    top_level_dir = '.'
-    src_dir = 'src'
+    top_level_dir = "."
+    src_dir = "src"
 
-    rename_src_directory(package_name)
     replace_in_python_files(src_dir, package_name)
+    rename_src_directory(src_dir, package_name)
     replace_package_name_placeholder(top_level_dir, package_name)
     print(f"Project configured with package name '{package_name}'")
+
 
 if __name__ == "__main__":
     main()
